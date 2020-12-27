b9834d518d0cd04764047e274bf73b5388180e25
LT <support@teran.tech>
2020-12-27 12:48:59 -0700

    Tutorial 4.1

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+   public float speed = 5.0f;
+
+   private Rigidbody playerRb;
+
+   // Start is called before the first frame update
+   void Start()
+   {
+      playerRb = GetComponent<Rigidbody>();
+   }
+
+   // Update is called once per frame
+   void Update()
+   {
+      float verticalInput = Input.GetAxis("Vertical");
+
+      playerRb.AddForce(Vector3.forward * verticalInput * speed);
+   }
+}
