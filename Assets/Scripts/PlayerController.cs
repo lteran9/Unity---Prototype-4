9fbac21a3e4acf8acbc81e6d53269212526e3440
LT <support@teran.tech>
2020-12-27 12:48:59 -0700

    Tutorial 4.1

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+   public float speed = 5.0f;
+
+   private GameObject focalPoint;
+   private Rigidbody playerRb;
+
+   // Start is called before the first frame update
+   void Start()
+   {
+      playerRb = GetComponent<Rigidbody>();
+      focalPoint = GameObject.Find("Focal Point");
+   }
+
+   // Update is called once per frame
+   void Update()
+   {
+      float verticalInput = Input.GetAxis("Vertical");
+
+      playerRb.AddForce(focalPoint.transform.forward * verticalInput * speed);
+   }
+}
