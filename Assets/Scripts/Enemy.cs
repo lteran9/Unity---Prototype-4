d9d20c98e50043e48e9a447ad28030e2a6b76a54
LT <support@teran.tech>
2020-12-28 09:13:28 -0700

    Tutorial 4.2

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+   private float speed = 3.0f;
+   private Rigidbody enemyRb; 
+   private GameObject player;
+
+   // Start is called before the first frame update
+   void Start()
+   {
+      enemyRb = GetComponent<Rigidbody>();
+      player = GameObject.Find("Player");
+   }
+
+   // Update is called once per frame
+   void Update()
+   {
+      // Get the Vector3 for the force direction
+      var lookDirection = (player.transform.position - transform.position).normalized;
+
+      enemyRb.AddForce(lookDirection * speed);
+   }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+   public GameObject enemyPrefab;
+
+   // Start is called before the first frame update
+   void Start()
+   {
+      Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+   }
+
+   // Update is called once per frame
+   void Update()
+   {
+
+   }
+
+   Vector3 GenerateSpawnPosition()
+   {
+      float spawnPosX = Random.Range(-9, 9);
+      float spawnPosY = Random.Range(-9, 9);
+
+      return new Vector3(spawnPosX, 0.15f, spawnPosY);
+   }
+}
